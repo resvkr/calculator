53c41d40a3338201f8e4d7c959045608fb125d43
Zenia <eugeniababak@gmail.com>
2024-05-09 16:45:28 +0200

    new calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,11 +4,11 @@ public class Calculator {
 
         switch (operation_temp) {
             case "+" -> result_temp = add(a, b);
-            case "-" -> result_temp = a - b;
-            case "*" -> result_temp = a * b;
-            case "/" -> result_temp = a / b;
-            case "%" -> result_temp = a % b;
-            case "^" -> result_temp = Math.pow(a, b);
+            case "-" -> result_temp = subtract(a, b);
+            case "*" -> result_temp = multiply(a, b);
+            case "/" -> result_temp = divide(a, b);
+            case "%" -> result_temp = module(a, b);
+            case "^" -> result_temp = pow(a, b);
             default -> System.out.println("Wrong parametr");
         }
         return result_temp;
@@ -17,4 +17,25 @@ public class Calculator {
     double add(double a, double b) {
         return a + b;
     }
+
+    double subtract(double a, double b) {
+        return a - b;
+    }
+
+    double multiply(double a, double b) {
+        return a * b;
+    }
+
+    double divide(double a, double b) {
+        return a / b;
+    }
+
+    double module(double a, double b) {
+        return a % b;
+    }
+
+    double pow(double a, double b) {
+        return Math.pow(a, b);
+    }
+
 }
